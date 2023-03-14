b58c83b756dc7ad9c35a3c90780360cc6aa1a9e1
Mateus <mateuscorreiaazevedo@gmail.com>
2023-03-13 21:29:02 -0300

    feat: add lib toastfy on boilerplate

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
 import '@/styles/globals.css'
 
 export default function App ({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
+      <ToastContainer />
     </SessionProvider>
   )
 }
